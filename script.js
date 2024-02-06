ae75ca8816381dc9482851efb158b2bba5b6e541
jmatzko <145479293+jmatzko@users.noreply.github.com>
2024-02-05 21:31:07 -0500

    UI done

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function multiply(a, b) {
 function divide(a,b) {
     return a / b;
 }
+
+function operate(operator, num1, num2) {
+    add(num1, num2);
+}
+
+const buttons = document.querySelectorAll('[class ^= btn]')
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log('clicked');
+    });
+});
+
